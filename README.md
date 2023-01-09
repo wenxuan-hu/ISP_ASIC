8b9cf4a1d1f24faf1f62b66082e3be82d56b62ca
Wenxuan Hu <huwe0427@vlsi031.ee.washington.edu>
2023-01-09 16:09:07 +0000

    DE 0.9

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ RTL for ISP
 
 
 histogram calculation for RGB image.
+
+
+hist_stat :  for better power,
+hist_stat_new:  for better area,
+
+
--- hist_stat_new.v
+++ hist_stat_new.v
@@ -1,6 +1,6 @@
 //  ******************************************************
 //  Author            : Wenxuan Hu
-//  Last modified     : 2023-01-06 02:26
+//  Last modified     : 2023-01-06 02:35
 //  Email             : huwe0427@uw.edu
 //  Student Number    : 2027482
 //  Filename          : hist_stat.v
@@ -8,7 +8,7 @@
 //    Copyright [2023] <Copyright Wenxuan Hu>
 //  ******************************************************
 
-// Jian Liang's version
+//  another version for signle port SRAM and regfile
 // ----------------------------------------------------
 //   2 pixel per cycle  : horizon direction
 //   sub-sample: average
@@ -72,25 +72,28 @@ module hist_stat(
   // ----------------------------------------------------
   //mem 
   //sram //TODO 
-  //address 7 bits for 1 block; max 16 blocks -> 11 bits; 128 level
-  output [10:0] addr_wr_o, 
-  output [10:0] addr_rd_o, 
-  output wr_wen_o,
-  output rd_wen_o,
-  output wr_cen_o,
-  output rd_cen_o,
-  output  [7:0] data_wr_o,
-  input  [7:0] data_rd_i,
-
+  // 128 level table, 8 bits per entry
+  //
+  // 4 SRAMs in total, 2 per block line,  sram width 16 bits:  8*64*16bits
+  // addr: 3+6 
+  output [8:0] addr_o, 
+  output  wen,
+  output  cen,
+  output  [15:0] data_wr_o,
+  input  [15:0] data_rd_i,
+  output sram_toggle,
   //register file
-  output [10:0] reg_addr_wr_o, 
-  output [10:0] reg_addr_rd_o, 
+  //  4 regfiles, 2 per block line
+  //  8*128 level * 8bits
+  output [9:0] reg_addr_wr_o, 
+  output [9:0] reg_addr_rd_o, 
   output  [7:0] reg_data_wr_o,
   input  [7:0]  reg_data_rd_i,
   output  reg_enable,
+  output reg_toggle, // for adjecent block
 
   //memory ping-pang toggle
-  output mem_toggle 
+  output mem_toggle  //for block line 
   // ----------------------------------------------------
 
 );
@@ -128,20 +131,30 @@ wire  [11:0] counter_v_nxt;
 
 // current block location
 wire [3:0] cur_block;
+wire [3:0] pre_block;
 
 //memory ctrl
 wire [7:0]  pre_counter_lo;
 wire [7:0]  counter_nxt_lo;
 
-wire [7:0]  pre_counter_hi;
-wire [7:0]  counter_nxt_hi;
+wire [15:0]  pre_counter_hi;
+wire [15:0]  counter_nxt_hi;
 
-reg   [10:0]  addr_rd_q; // sram wr addr;
+reg   [8:0]  addr_rd_q; // sram wr addr;
+
+wire  sram_rd;
+reg  sram_wr;
+
 
 // one bit for carry logic
 wire carry;   
 reg carry_q;
 
+reg [127:0] carry_mem_a;
+reg [127:0] carry_mem_b;
+
+wire [1:0]  carry_clear;
+wire [6:0] carry_counter; // counting for move the carry bit to sram.
 
 // ----------------------------------------------------
 // hand shake
@@ -202,6 +215,7 @@ assign counter_v_nxt  = (counter_h == (frame_width_i-'d1)) ?   (  (counter_v  ==
 
 
 assign cur_block = counter_h[11:8];
+assign pre_block = cur_block=='d0  ?  ( counter_v < frame_width ? 'd0  : block_num_h_i )  : cur_block  - 1'b1 ;
 // toggle for memory; dont need to delay
 // pixel line:  line blanking time-----> pixel valid time ----> line blanking time
 assign mem_toggle = counter_v[8];
@@ -227,47 +241,95 @@ end
 // ----------------------------------------------------
 // update low-bits parts-> reg file
 
-assign reg_addr_rd_o  = {cur_block, luma_avg[7:1]};
+assign reg_addr_rd_o  = {cur_block[3:1], luma_avg[7:1]};
 assign pre_counter_lo  = reg_data_rd_i;
 
 assign {carry, counter_nxt_lo}  = pre_counter_lo + 1'b1;
-assign reg_addr_wr_o  = {cur_block, luma_avg[7:1]};
+assign reg_addr_wr_o  = {cur_block[3:1], luma_avg[7:1]};
 assign reg_data_wr_o  = counter_nxt_lo;
 
 assign reg_enable  = get_d1;
 
+assign reg_toggle = cur_block[0]; 
+
+
 // ----------------------------------------------------
 // update the high-bits parts -> SRAM
-always@(posedge pclk or negedge rst_n)begin
-  if(~rst_n)
-    carry_q <=  1'b0;
-  else 
-    carry_q <=  carry;
+always@(posedge pclk or negedge rst_n)  begin
+  if (~rst_n)
+    dummy_counter <=  'd0;
+  else if(dummy_counter == 'd127)
+    dummy_counter <=  'd0;
+  else if(dummy_counter == 'd1)
+    dummy_counter <=  dummy_counter + 1'b1;
+  else if(pre_block == block_num_h_i  & counter_h[6:0] == 'd0)
+    dummy_counter <=  dummy_counter + 1'b1;
 end
 
-assign rd_wen_o = carry;
-assign rd_cen_o = ~carry;
-assign wr_wen_o = ~carry_q;
-assign wr_cen_o = ~carry_q;
 
-assign addr_rd_o  = carry?  {cur_block, luma_avg[7:1]} : 'd0;
-assign addr_wr_o  = addr_rd_q;
+assign carry_counter = pre_counter == block_num_h_i  ?dummy_counter  : counter_h[6:0];
+
+always @(posedge pclk or negedge rst_n) begin
+  if (~rst_n)
+    carry_mem_a <=  128'd0;
+  else if (~reg_toggle & reg_enable & carry)
+    carry_mem_a[luma[7:1]]  <=  1'b1;
+  else if (reg_toggle & carry_clear[0])
+    carry_mem_a[{carry_counter[6:1], 1'b0}]  <=  1'b0;
+  else if (reg_toggle & carry_clear[1])
+    carry_mem_a[{carry_counter[6:1], 1'b1}]  <=  1'b0;
+end
+always @(posedge pclk or negedge rst_n) begin
+  if (~rst_n)
+    carry_mem_b <=  128'd0;
+  else if (reg_toggle & reg_enable & carry)
+    carry_mem_b[luma[7:1]]  <=  1'b1;
+  else if (~reg_toggle & carry_clear[0])
+    carry_mem_b[{carry_counter[6:1], 1'b0}]  <=  1'b0;
+  else if (~reg_toggle & carry_clear[1])
+    carry_mem_b[{carry_counter[6:1], 1'b1}]  <=  1'b0;
+end
+
+
+
+
+
+assign wen  = ~sram_wr;
+assign cen  = ~(sram_wr|sram_rd);
+
+assign carry_clear[1] = sram_toggle  ? carry_mem_a[{carry_counter[6:1], 1'b1}] : carry_mem_b[{carry_counter[6:1], 1'b1}];
+assign carry_clear[0] = sram_toggle  ? carry_mem_a[{carry_counter[6:1], 1'b0}] : carry_mem_b[{carry_counter[6:1], 1'b0}];
+
+
+assign sram_rd  = | carry_clear;
+always @(posedge pclk or negedge rst_n)begin
+  if(~rst_n)
+    sram_wr <=  1'b0;
+  else 
+    sram_wr <=  sram_rd;
+end
+
+assign addr_o = {pre_block , carry_counter[6:1]};
 
 always@(posedge pclk or negedge rst_n)  begin
     if(~rst_n) 
       addr_rd_q <=  'd0;
-    else if(carry)
-      addr_rd_q <=  {cur_block, luma_avg[7:1]};
+    else if(|carry_clear)
+      addr_rd_q <=  {pre_block[3:1], carry_counter[6:1]};
 end
 
 
 assign pre_counter_hi = data_rd_i;
-assign counter_nxt_hi = pre_counter_hi  + 1'b1;
+assign counter_nxt_hi[7:0] = pre_counter_hi[7:0]  + 1'b1 : pre_counter_hi[7:0];
+assign counter_nxt_hi[15:8] = pre_counter_hi[15:8]  + 1'b1 : pre_counter_hi[15:8];
 
 assign data_wr_o  = counter_nxt_hi;
 
 
 // memory will be reset/clear by curve_calc module
+assign sram_toggle  = pre_block[0];
+
+
 
 
 endmodule
@@ -323,83 +385,139 @@ endmodule
 
 module acad_top();
 
-wire cen_rd_0 = rd_cen_o |  toggle;
-wire cen_rd_1 = rd_cen_o |  toggle;
-
-wire cen_wr_0 = wr_cen_o |  toggle;
-wire cen_wr_1 = wr_cen_o |  toggle;
-
-wire wen_wr_0 = wr_wen_o |  toggle;
-wire wen_wr_1 = wr_wen_o |  toggle;
+wire cen_0_a = cen_o_a |  toggle;
+wire cen_1_a = cen_o_a |  toggle;
+wire cen_0_b = cen_o_b |  toggle;
+wire cen_1_b = cen_o_b |  toggle;
 
 
-wire [7:0]  data_rd_0;
-wire [7:0]  data_rd_1;
+wire wen_0_b = wen_o_b |  toggle;
+wire wen_1_b = wen_o_b |  toggle;
 
-wire [7:0]  data_rd;
-assign data_rd = toggle ? data_rd_1 : data_rd_0;
+
+wire [15:0]  data_rd_0_a;
+wire [15:0]  data_rd_0_b;
+wire [15:0]  data_rd_1_a;
+wire [15:0]  data_rd_1_b;
+
+wire [15:0]  data_rd_a;
+wire [15:0]  data_rd_b;
+assign data_rd_a = toggle ? data_rd_1_a : data_rd_0_a;
+assign data_rd_b = toggle ? data_rd_1_b : data_rd_0_b;
 
 // memory can be read by hist module and curve_calc module
 // memory can be write by hist module, reset by curve_calc
 
 // 1w1r sram
-sram_1r1w inst_0(
+sram_1r1w inst_0_a(
   .clk(pclk),
   .rst_n(rst_n),
   
-  .cen_rd(cen_rd_0 & curve_calc_read_logic),
-  .cen_wr(cen_wr_0),
-  .wen_wr(wen_wr_0),
+  .cen_rd(cen_rd_0_a & curve_calc_read_logic),
+  .wen_wr(wen_wr_0_a),
 
-  .addr_rd(addr_rd   & curve_calc_read_addr),
-  .data_rd(data_rd_0),
-  .addr_wr(addr_wr   & curve_calc_write_addr),
+  .data_rd(data_rd_0_a),
+  .addr(addr_wr   & curve_calc_write_addr),
   .data_wr(data_wr)
 );
-
-sram_1r1w inst_1(
+sram_1r1w inst_1_a(
   .clk(pclk),
   .rst_n(rst_n),
   
-  .cen_rd(cen_rd_1 & curve_calc_read_logic),
-  .cen_wr(cen_wr_1),
-  .wen_wr(wen_wr_1),
+  .cen_rd(cen_rd_1_a & curve_calc_read_logic),
+  .wen_wr(wen_wr_1_a),
 
-  .addr_rd(addr_rd   & curve_calc_read_addr),
-  .data_rd(data_rd_1),
-  .addr_wr(addr_wr   & curve_calc_write_addr),
+  .data_rd(data_rd_1_a),
+  .addr(addr_wr   & curve_calc_write_addr),
+  .data_wr(data_wr)
+);
+sram_1r1w inst_0_b(
+  .clk(pclk),
+  .rst_n(rst_n),
+  
+  .cen_rd(cen_rd_0_b & curve_calc_read_logic),
+  .wen_wr(wen_wr_0_b),
+
+  .data_rd(data_rd_0_b),
+  .addr(addr_wr   & curve_calc_write_addr),
+  .data_wr(data_wr)
+);
+sram_1r1w inst_1_b(
+  .clk(pclk),
+  .rst_n(rst_n),
+  
+  .cen_rd(cen_rd_1_b & curve_calc_read_logic),
+  .wen_wr(wen_wr_1_b),
+
+  .data_rd(data_rd_1_b),
+  .addr(addr_wr   & curve_calc_write_addr),
   .data_wr(data_wr)
 );
 
+// ----------------------------------------------------
+// reg
 
-wire [7:0]  reg_data_rd_0;
-wire [7:0]  reg_data_rd_1;
+wire [7:0]  reg_data_rd_0_a;
+wire [7:0]  reg_data_rd_1_a;
 
-wire [7:0]  reg_data_rd;
-assign reg_data_rd = toggle ? reg_data_rd_1 : reg_data_rd_0;
+wire [7:0]  reg_data_rd_a;
+assign reg_data_rd_a = toggle ? reg_data_rd_1_a : reg_data_rd_0_a;
 //regfile
-regfile inst_2(
+regfile inst_2_a(
   .cll(pclk),
   .rst_n(rst_n),
 
   .addr_wr(reg_addr_wr),
   .addr_rd(reg_addr_rd),
   .data_wr(reg_data_wr),
-  .data_rd(reg_data_rd_0)
+  .data_rd(reg_data_rd_0_a)
 
-  .enanle(~toggle & reg_enable |  curve_calcu_enable_logic)
+  .enanle(~toggle & ~reg_toggle & reg_enable |  curve_calcu_enable_logic)
 );
 
-regfile inst_3(
+regfile inst_3_a(
   .cll(pclk),
   .rst_n(rst_n),
 
   .addr_wr(reg_addr_wr),
   .addr_rd(reg_addr_rd),
   .data_wr(reg_data_wr),
-  .data_rd(reg_data_rd_1)
+  .data_rd(reg_data_rd_1_a)
 
-  .enanle(toggle & reg_enable |  curve_calcu_enable_logic)
+  .enanle(toggle & reg_toggle& reg_enable |  curve_calcu_enable_logic)
 );
+
+
+
+wire [7:0]  reg_data_rd_0_b;
+wire [7:0]  reg_data_rd_1_b;
+
+wire [7:0]  reg_data_rd_b;
+assign reg_data_rd_b = toggle ? reg_data_rd_1_b : reg_data_rd_0_b;
+//regfile
+regfile inst_2_b(
+  .cll(pclk),
+  .rst_n(rst_n),
+
+  .addr_wr(reg_addr_wr),
+  .addr_rd(reg_addr_rd),
+  .data_wr(reg_data_wr),
+  .data_rd(reg_data_rd_0_b)
+
+  .enanle(~toggle & reg_toggle& reg_enable |  curve_calcu_enable_logic)
+);
+
+regfile inst_3_b(
+  .cll(pclk),
+  .rst_n(rst_n),
+
+  .addr_wr(reg_addr_wr),
+  .addr_rd(reg_addr_rd),
+  .data_wr(reg_data_wr),
+  .data_rd(reg_data_rd_1_b)
+
+  .enanle(toggle & reg_toggle & reg_enable |  curve_calcu_enable_logic)
+);
+
 
 endmodule
